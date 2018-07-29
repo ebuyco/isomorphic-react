0bff4d22e2af41c0d30480ad8da96637e773173c
Ernie Buyco <ernie.buyco@fmclarity.com>
2018-07-29 19:11:48 +0800

    test_update_react_isomorphic

--- server/index.jsx
+++ server/index.jsx
@@ -1,13 +1,26 @@
 import express from 'express';
 import yields from 'express-yields';
 import fs from 'fs-extra';
+import webpack from 'webpack';
+import { argv } from 'optimist';
 
 const port = process.env.Port || 3000;
 const app = express();
 
-app.get(['/'], function * (req,res){
-    let index = yield fs.readFile('./public/index.html', "utf-8");
+if (process.env.NODE_ENV === 'development') {
+    const config = require('../webpack.config.dev.babel').default;
+    const compiler = webpack(config);
+
+    app.use(require('webpack-dev-middleware') (compiler, {
+        noInfo:true
+    }));
+
+    app.use(require('webpack-hot-middleware') (compiler));
+}
+
+app.get(['/'], function* (req, res) {
+    const index = yield fs.readFile('./public/index.html', 'utf-8');
     res.send(index);
 });
 
-app.listen(port, '0.0.0.0', () =>console.info(`App listening on ${port}`));
+app.listen(port, '0.0.0.0', () => console.info(`App listening on ${port}`));
